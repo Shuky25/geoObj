acd4e8d5b963536f6fa0f08bff4091e9940fb586
Programiranje <strahinjasretovic04@gmail.com>
2022-11-07 11:26:38 +0100

    GeometrijskiObjekat

--- GeometrijskiObjekat.java
+++ GeometrijskiObjekat.java
@@ -1,3 +1,21 @@
-public class GeometrijskiObjekat {
+public abstract class GeometrijskiObjekat {
+    Tacka t;
+    public GeometrijskiObjekat() {
+        t.setX(0);
+        t.setY(0);
+    }
+    public GeometrijskiObjekat(int x, int y) {
+        t.setX(x);
+        t.setY(y);
+    }
+    public GeometrijskiObjekat(Tacka a) {
+        t.setX(a.getX());
+        t.setY(a.getY());
+    }
+
+    public abstract int Povrsina();
+    public String Ispis() {
+     return "Tacka(" + t.getX() + ", " + t.getY() + ")";
+    }
 
 }
